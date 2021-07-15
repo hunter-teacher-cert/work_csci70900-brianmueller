cebffb9342fd4199a647ac98698b7a1da322dd4f
Brian Mueller <brianscottmueller@gmail.com>
2021-07-15 09:47:39 -0400

    didn't save

--- ds/lists/Driver.java
+++ ds/lists/Driver.java
@@ -1,89 +1,114 @@
 import java.io.*;
 import java.util.*;
 
+public class Driver{
+  public static void main(String[] args) {
+    // DEMO
+    // Node n;
+    // n = new Node();
+    // n.setData("Eduardo");
+    //
+    //
+    // Node n2 = new Node("Brian");
+    // n.setNext(n2);
+    //
+    // System.out.println(n);
+    //
+    // System.out.println(n2);
+    //
+    // System.out.println(n.getNext());
+    // n.getNext().setNext(new Node("Steph"));
+    // System.out.println(n2.getNext());
+    // System.out.println(n.getNext().getNext());
 
-public class Llist{
-  private Node front; // the front of the list
+    // TASK
+    Node L = new Node();
 
-  public  Llist(){
-    front = null;
+    // 1.Create a new  list that looks like:
+    //   L->"a"->"b"->"c"->"d"
+    Node d = new Node("d");
+    Node c = new Node("c",d);
+    Node b = new Node("b",c);
+    Node a = new Node("a",b);
+    L.setNext(a);
+
+    // ONE way of printing
+    // System.out.println(L); // null
+    // System.out.println(L.getNext()); // a
+    // System.out.println(L.getNext().getNext()); // b
+    // System.out.println(L.getNext().getNext().getNext()); // c
+    // System.out.println(L.getNext().getNext().getNext().getNext()); // d
+
+    // ANOTHER way of printing
+    // Node tempNode = L;
+    // while(tempNode != null){
+    //   System.out.print(tempNode + " --> "); // prints extra -->
+    //   tempNode = tempNode.getNext();
+    // }
+
+
+
+
+
+
+
+
+
+    // 2. Write the code to insert an "x"
+    //    between the b and the c
+
+    Node x = new Node("x");
+    x.setNext(c);
+    b.setNext(x);
+
+    // ONE way of printing
+    // System.out.println(L); // null
+    // System.out.println(L.getNext()); // a
+    // System.out.println(L.getNext().getNext()); // b
+    // System.out.println(L.getNext().getNext().getNext()); // x
+    // System.out.println(L.getNext().getNext().getNext().getNext()); // c
+    // System.out.println(L.getNext().getNext().getNext().getNext().getNext()); // d
+
+    // ANOTHER way of printing
+    // Node tempNode = L;
+    // while(tempNode != null){
+    //   System.out.print(tempNode + " --> "); // prints extra -->
+    //   tempNode = tempNode.getNext();
+    // }
+
+
+
+
+
+
+    // 3. Write the code to delete the c
+    x.setNext(d);
+
+    // ONE way of printing
+    // System.out.println(L); // null
+    // System.out.println(L.getNext()); // a
+    // System.out.println(L.getNext().getNext()); // b
+    // System.out.println(L.getNext().getNext().getNext()); // x
+    // System.out.println(L.getNext().getNext().getNext().getNext()); // d
+
+    // ANOTHER way of printing
+    // Node tempNode = L;
+    // while(tempNode != null){
+    //   System.out.print(tempNode + " --> "); // prints extra -->
+    //   tempNode = tempNode.getNext();
+    // }
+
+    System.out.println("\n");
+  	System.out.println("Llist");
+  	System.out.println("-----------");
+
+  	Llist ll = new Llist();
+  	System.out.println(ll);
+
+    ll.addFront("c");
+    ll.addFront("b");
+    ll.addFront("a");
+    System.out.println(ll);
+
   }
-
-  // Add a new node containing data
-  // at the front of the list
-  public void addFront(String data){
-    // make the new node
-    Node newNode = new Node(data);
-    // point it to what front points to
-    newNode.setNext(front);
-    // point front to the new node
-    front = newNode;
-  }
-
-  public String toString(){
-    Node currentNode;
-    currentNode = front;
-    String result = "";
-    while (currentNode != null){
-      result = result + currentNode + "->";
-      // this is like i=i+1 is for arrays
-      // but for linked lists
-      currentNode = currentNode.getNext();
-    }
-    result = result + "null";
-    return result;
-
-  }
-
-  // returns True if there is nothing in the list
-  // False otherwise
-  public boolean isEmpty(){
-    return false;
-  }
-
-  // returns the number of items in the list
-  // Hint: look at the toString
-  // to remind you how to traverse down the list
-  public int length(){
-    return 0;
-  }
-
-  // returns the item at location index;
-  // returns null if there aren't enough
-  // items. Starts with index 0
-  public String get(int index){
-    return null;
-
-  }
-
-  // sets the item at location index (starting
-  // with 0) to value.
-  // only sets if the index is within range
-  public void set(int index, String value){
-
-  }
-
-  // insert an item before index.
-  // only inserts if the index is within bounds
-  // Hint: look at toString for hints on
-  // traversal and draw out a diagram.
-  // You will need a variable that refers to
-  // the node BEFORE you want to do the insertion.
-  public void insert(int index, String value){
-
-  }
-
-  // returns the index of the first item with
-  // data value key. Returns -1 if not found
-  public int search(String key){
-    return -1;
-  }
-  
-  // removes the node at index.
-  // does nothing if index out of bounds
-  public void remove(int index){
-
-  }
-
-
 }
