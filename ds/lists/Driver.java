893651e4f28974a5f2f4bf9018480356bae8931c
Brian Mueller <brianscottmueller@gmail.com>
2021-07-15 09:47:18 -0400

    oops

--- ds/lists/Driver.java
+++ ds/lists/Driver.java
@@ -3,87 +3,87 @@ import java.util.*;
 
 
 public class Llist{
-    private Node front; // the front of the list
+  private Node front; // the front of the list
 
-    public  Llist(){
-	front = null;
+  public  Llist(){
+    front = null;
+  }
+
+  // Add a new node containing data
+  // at the front of the list
+  public void addFront(String data){
+    // make the new node
+    Node newNode = new Node(data);
+    // point it to what front points to
+    newNode.setNext(front);
+    // point front to the new node
+    front = newNode;
+  }
+
+  public String toString(){
+    Node currentNode;
+    currentNode = front;
+    String result = "";
+    while (currentNode != null){
+      result = result + currentNode + "->";
+      // this is like i=i+1 is for arrays
+      // but for linked lists
+      currentNode = currentNode.getNext();
     }
+    result = result + "null";
+    return result;
 
-    // Add a new node containing data
-    // at the front of the list
-    public void addFront(String data){
-	// make the new node
-	Node newNode = new Node(data);
-	// point it to what front points to
-	newNode.setNext(front);
-	// point front to the new node
-	front = newNode;
-    }
+  }
 
-    public String toString(){
-	Node currentNode;
-	currentNode = front;
-	String result = "";
-	while (currentNode != null){
-	    result = result + currentNode + "->";
-	    // this is like i=i+1 is for arrays
-	    // but for linked lists
-	    currentNode = currentNode.getNext();
-	}
-	result = result + "null";
-	return result;
+  // returns True if there is nothing in the list
+  // False otherwise
+  public boolean isEmpty(){
+    return false;
+  }
 
-    }
+  // returns the number of items in the list
+  // Hint: look at the toString
+  // to remind you how to traverse down the list
+  public int length(){
+    return 0;
+  }
 
-    // returns True if there is nothing in the list
-    // False otherwise
-    public boolean isEmpty(){
-	return false;
-    }
+  // returns the item at location index;
+  // returns null if there aren't enough
+  // items. Starts with index 0
+  public String get(int index){
+    return null;
 
-    // returns the number of items in the list
-    // Hint: look at the toString
-    // to remind you how to traverse down the list
-    public int length(){
-	return 0;
-    }
+  }
 
-    // returns the item at location index;
-    // returns null if there aren't enough
-    // items. Starts with index 0
-    public String get(int index){
-	return null;
+  // sets the item at location index (starting
+  // with 0) to value.
+  // only sets if the index is within range
+  public void set(int index, String value){
 
-    }
+  }
 
-    // sets the item at location index (starting
-    // with 0) to value.
-    // only sets if the index is within range
-    public void set(int index, String value){
+  // insert an item before index.
+  // only inserts if the index is within bounds
+  // Hint: look at toString for hints on
+  // traversal and draw out a diagram.
+  // You will need a variable that refers to
+  // the node BEFORE you want to do the insertion.
+  public void insert(int index, String value){
 
-    }
+  }
 
-    // insert an item before index.
-    // only inserts if the index is within bounds
-    // Hint: look at toString for hints on
-    // traversal and draw out a diagram.
-    // You will need a variable that refers to
-    // the node BEFORE you want to do the insertion.
-    public void insert(int index, String value){
+  // returns the index of the first item with
+  // data value key. Returns -1 if not found
+  public int search(String key){
+    return -1;
+  }
+  
+  // removes the node at index.
+  // does nothing if index out of bounds
+  public void remove(int index){
 
-    }
-
-    // returns the index of the first item with
-    // data value key. Returns -1 if not found
-    public int search(String key){
-	return -1;
-    }
-
-    // removes the node at index.
-    // does nothing if index out of bounds
-    public void remove(int index){
-
-    }
+  }
 
 
 }
