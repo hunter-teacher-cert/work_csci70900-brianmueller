82b5a2bb4d3263d48c7869828554926a6c2139a0
Brian Mueller <brianscottmueller@gmail.com>
2021-07-21 09:31:54 -0600

    dlist

---
+++ ds/dlist/DlistDriver.java
@@ -0,0 +1,29 @@
+/**
+* Team 1
+* Victoria B.
+* Brian M.
+* Eric Liu
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class DlistDriver{
+  public static void main(String[] args) {
+
+    Node a = new Node("a");
+    Node b = new Node("b");
+    Node c = new Node("c");
+
+    Dlist n = new Dlist();
+    n.addFront(b);
+    n.addFront(a);
+    n.addBack(c);
+    System.out.println(n);  // expected output is a b c
+    //Remove b
+    n.remove(1);
+    System.out.println(n);  // expected output is a c
+
+
+  }//end main()
+}//end Driver class
